c2824840f4021bba4a762fbdfc45dba582505943
soninaren <nasoni@microsoft.com>
2019-11-20 15:01:09 -0800

    updating build.ps1

--- build.ps1
+++ build.ps1
@@ -28,9 +28,7 @@ else {
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     Invoke-Expression -Command  "dotnet run --sign"
-    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
 else {
     Invoke-Expression -Command  "dotnet run"
-    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
